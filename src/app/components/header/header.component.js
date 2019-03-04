962ad16bc59f247e9f952623f52c732a9fc97133
David <davidfloyd322@gmail.com>
2019-03-04 16:29:35 +0200

    header, add header to project

---
+++ src/app/components/header/header.component.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import headerTpl from './header.html';
+import HeaderController from './header.controller';
+
+export default class HeaderComponent {
+    constructor() {
+        this.templateUrl = headerTpl;
+        this.controller = HeaderController;
+    }
+}
--- src/app/index.components.js
+++ src/app/index.components.js
@@ -1,7 +1,9 @@
 'use strict';
 
 import footerModule from './components/footer/footer.module';
+import headerModule from './components/header/header.module';
 
 export default angular.module('index.components', [
-	footerModule.name
+	footerModule.name,
+	headerModule.name
 ]);
