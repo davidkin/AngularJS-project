3c8ddb2bcd8002b126d53af484f7709015e5145f
David <davidfloyd322@gmail.com>
2019-03-05 12:39:17 +0200

    refactor/index.run, refactor code

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -3,15 +3,17 @@
 function runBlock($window, $rootScope) {
 	'ngInject';
 	$rootScope.netActive = navigator.onLine;
+	
 	$window.addEventListener('offline', function() {
-	$rootScope.$apply(function() {
-		$rootScope.netActive = false;
+		$rootScope.$apply(function() {
+			$rootScope.netActive = false;
+		});
 	});
-	});
+
 	$window.addEventListener('online', function() {
-	$rootScope.$apply(function() {
-		$rootScope.netActive = true;
-	});
+		$rootScope.$apply(function() {
+			$rootScope.netActive = true;
+		});
 	});
 }
 
